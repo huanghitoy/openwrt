e3645a84caf1927412c5760f3fe0ebd803e8e4de
huanghitoy <huanghitoy@gmail.com>
2024-07-31 21:32:41 +0800

    Update mbim.sh
    Signed-off-by: huanghitoy <huanghitoy@gmail.com>

--- package/network/utils/umbim/files/lib/netifd/proto/mbim.sh
+++ package/network/utils/umbim/files/lib/netifd/proto/mbim.sh
@@ -14,12 +14,11 @@ proto_mbim_init_config() {
 	proto_config_add_string apn
 	proto_config_add_string pincode
 	proto_config_add_string delay
+	proto_config_add_boolean allow_roaming
+	proto_config_add_boolean allow_partner
 	proto_config_add_string auth
 	proto_config_add_string username
 	proto_config_add_string password
-	proto_config_add_string pdptype
-	proto_config_add_string ip4table
-	proto_config_add_string ip6table
 	proto_config_add_defaults
 }
 
@@ -28,8 +27,8 @@ _proto_mbim_setup() {
 	local tid=2
 	local ret
 
-	local device apn pincode delay pdptype ip4table ip6table $PROTO_DEFAULT_OPTIONS
-	json_get_vars device apn pincode delay auth username password pdptype ip4table ip6table $PROTO_DEFAULT_OPTIONS
+	local device apn pincode delay allow_roaming allow_partner $PROTO_DEFAULT_OPTIONS
+	json_get_vars device apn pincode delay auth username password allow_roaming allow_partner $PROTO_DEFAULT_OPTIONS
 
 	[ -n "$ctl_device" ] && device=$ctl_device
 
@@ -111,14 +110,31 @@ _proto_mbim_setup() {
 	tid=$((tid + 1))
 
 	echo "mbim[$$]" "Register with network"
-	umbim $DBG -n -t $tid -d $device registration || {
-		echo "mbim[$$]" "Subscriber registration failed"
+	connected=0
+	umbim $DBG -n -t $tid -d $device registration
+	reg_status=$?
+	case $reg_status in
+		0)	echo "mbim[$$]" "Registered in home mode"
+			tid=$((tid + 1))
+			connected=1;;
+		4)	if [ "$allow_roaming" = "1" ]; then
+				echo "mbim[$$]" "Registered in roaming mode"
+				tid=$((tid + 1))
+				connected=1
+			fi;;
+		5) 	if [ "$allow_partner" = "1" ]; then
+				echo "mbim[$$]" "Registered in partner mode"
+				tid=$((tid + 1))
+				connected=1
+			fi;;
+	esac
+	if [ $connected -ne 1 ]; then
+		echo "mbim[$$]" "Subscriber registration failed (code $reg_status)"
 		tid=$((tid + 1))
 		umbim $DBG -t $tid -d "$device" disconnect
 		proto_notify_error "$interface" NO_REGISTRATION
 		return 1
-	}
-	tid=$((tid + 1))
+	fi
 
 	echo "mbim[$$]" "Attach to network"
 	umbim $DBG -n -t $tid -d $device attach || {
@@ -131,7 +147,7 @@ _proto_mbim_setup() {
 	tid=$((tid + 1))
 
 	echo "mbim[$$]" "Connect to network"
-	while ! umbim $DBG -n -t $tid -d $device connect "$apn" "$pdptype" "$auth" "$username" "$password"; do
+	while ! umbim $DBG -n -t $tid -d $device connect "$apn" "$auth" "$username" "$password"; do
 		tid=$((tid + 1))
 		sleep 1;
 	done
@@ -139,36 +155,26 @@ _proto_mbim_setup() {
 
 	uci_set_state network $interface tid "$tid"
 
-	pdptype="$(echo "$pdptype" | awk '{print tolower($0)}')"
-
-	[ "$pdptype" = "ip" -o "$pdptype" = "ipv6" -o "$pdptype" = "ipv4v6" ] || pdptype="ip"
-
 	echo "mbim[$$]" "Connected, starting DHCP"
 	proto_init_update "$ifname" 1
 	proto_send_update "$interface"
 
-	[ "$pdptype" = "ip" -o "$pdptype" = "ipv4v6" ] && {
-		json_init
-		json_add_string name "${interface}_4"
-		json_add_string ifname "@$interface"
-		json_add_string proto "dhcp"
-		json_add_string ip4table "$ip4table"
-		proto_add_dynamic_defaults
-		json_close_object
-		ubus call network add_dynamic "$(json_dump)"
-	}
+	json_init
+	json_add_string name "${interface}_4"
+	json_add_string ifname "@$interface"
+	json_add_string proto "dhcp"
+	proto_add_dynamic_defaults
+	json_close_object
+	ubus call network add_dynamic "$(json_dump)"
 
-	[ "$pdptype" = "ipv6" -o "$pdptype" = "ipv4v6" ] && {
-		json_init
-		json_add_string name "${interface}_6"
-		json_add_string ifname "@$interface"
-		json_add_string proto "dhcpv6"
-		json_add_string ip6table "$ip6table"
-		json_add_string extendprefix 1
-		proto_add_dynamic_defaults
-		json_close_object
-		ubus call network add_dynamic "$(json_dump)"
-	}
+	json_init
+	json_add_string name "${interface}_6"
+	json_add_string ifname "@$interface"
+	json_add_string proto "dhcpv6"
+	json_add_string extendprefix 1
+	proto_add_dynamic_defaults
+	json_close_object
+	ubus call network add_dynamic "$(json_dump)"
 }
 
 proto_mbim_setup() {
